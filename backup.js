f88efbda59728c35858a4a033402ccaf64c1f1af
Andrew Moss <andrewmoss@Andrews-MacBook-Pro.local>
2021-02-20 21:39:13 -0600

    created dynamic person filmGetter

--- backup.js
+++ backup.js
@@ -1,3 +1,28 @@
+const fs = require('fs');
 const fetch = require('node-fetch')
 
+function getter(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(person => {
+            console.log(person.name)
+            person.films.forEach((film) => {
+                filmGetter(film)
+            })
 
+        })
+}
+
+function filmGetter(filmUrl) {
+    fetch(filmUrl)
+        .then(res => res.json())
+        .then(film => console.log(film.title))
+}
+function personFilms(personId) {
+
+    getter(`https://swapi.dev/api/people/${personId}/`)
+
+}
+
+personFilms(4)
+// getter('https://swapi.dev/api/people/1/')
